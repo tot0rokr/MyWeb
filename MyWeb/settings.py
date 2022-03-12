764cec336becfad56291befc6366c591c016f8e1
TOT0RoKR <tot0roprog@gmail.com>
2022-03-12 17:35:20 +0000

    Link with Nginx, MariaDB

--- MyWeb/settings.py
+++ MyWeb/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = [".run.goorm.io"]
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -76,9 +75,17 @@ WSGI_APPLICATION = 'MyWeb.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', # mysqlclient 패키지 설치
+        'NAME': 'lunchbox',
+        'USER': 'lunchbox_user',
+        'PASSWORD': 'lunchbox_user',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
